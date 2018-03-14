d60bba6966bb743fc623d7e74e15f76e1d317ed5
Eric Adamski <er.adamski@gmail.com>
2018-03-14 07:39:24 -0400

    Allow query params

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import GifSearch from "react-gif-search";
+import qs from "qs";
+
 import { Page, Title, Arrow } from "./page.styled";
 
 class App extends Component {
+  state = { query: null };
+
+  componentDidMount() {
+    this.setState({ query: qs.parse(window.location.search.substring(1)).gif });
+  }
+
   render() {
+    const { query } = this.state;
+
     return (
       <Page>
         <Title>Search for some gifs!</Title>
         <Arrow />
-        <GifSearch />
+        <GifSearch query={query} />
       </Page>
     );
   }
